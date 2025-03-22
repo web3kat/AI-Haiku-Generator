d1c4405f9bdd5ad9b0351f4474e321b7dc2948bf
Kat <kartmohd@gmail.com>
2025-03-23 02:11:11 +0800

    Added JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generateHaiku(event) {
+  event.preventDefault();
+
+  new Typewriter("#haiku", {
+    strings: "A world of dew",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let haikuFormElement = document.querySelector("#haiku-generator-form");
+haikuFormElement.addEventListener("submit", generateHaiku);
