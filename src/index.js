c2cc0a538a16a704240e018aead5690f822b06ab
Kat <kartmohd@gmail.com>
2025-03-23 04:44:12 +0800

    Improved UX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayHaiku(response) {
-  console.log("haiku generated");
   new Typewriter("#haiku", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,9 +16,9 @@ function generateHaiku(event) {
     "A haiku is a traditional Japanese poem which follows a 5-7-5 syllable pattern and is typically unrhymed. You are a haiku expert. Your mission is to generate a haiku based on the user input. Do not include a title. Please separate each line with a <br />. Please sign the haiku with 'SheCodes AI' inside a <strong> element in a new line after the end of the haiku.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${API_KEY}`;
 
-  console.log("Generating haiku...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${prompt}`);
+  let haikuElement = document.querySelector("#haiku");
+  haikuElement.classList.remove("hidden");
+  haikuElement.innerHTML = `<div class="blink">⏳ Generating a haiku about ${instructionInput.value}</div>`;
 
   axios.get(apiUrl).then(displayHaiku);
 }
